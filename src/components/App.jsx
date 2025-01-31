8169635ab602680b13a9e4504ded9e42338601d6
Mohammad A <mohammarar03@gmail.com>
2025-01-31 19:17:18 +0200

    add note functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+
+  const [noteArr,setNoteArr] = useState([]);
+
+  function addNote2(note){
+    setNoteArr(prevNotes=>{
+      return [...prevNotes,note];
+    })
+  }
+
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea onAdd={addNote2} />
+     { noteArr.map((noteItem) =>{
+        return <Note
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      })}
+
       <Footer />
     </div>
   );
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+
+
+  const [note, setNote] =  useState({title:"",content:""});
+
+  function hChange(event){
+    const{name, value} = event.target;
+
+    setNote(prevNote =>{
+      return{
+        ...prevNote,[name]:value
+      };
+    });
+  }
+
+  function addNote(event){
+    props.onAdd(note);
+    event.preventDefault();
+
+    setNote({title: "", content: ""});
+    
+  }
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input onChange={hChange} value={note.title} name="title" placeholder="Title" />
+        <textarea onChange={hChange} value={note.content} name="content" placeholder="Take a note..." rows="3" />
+        <button onClick={addNote}>Add</button>
       </form>
     </div>
   );
